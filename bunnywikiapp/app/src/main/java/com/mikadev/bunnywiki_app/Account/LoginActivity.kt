55e53f29c7b29a1adb244610213123ad24fbb360
mwiwema <mwiwema09@gmail.com>
2021-01-06 16:16:33 +0100

    Bunnywiki_app gemaakt
    Opzet van applicatie (files etc)
    * Gestart met 'login_activity'

---
+++ bunnywikiapp/app/src/main/java/com/mikadev/bunnywiki_app/Account/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.mikadev.bunnywiki_app.bunnywiki_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mikadev.bunnywiki_app.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
