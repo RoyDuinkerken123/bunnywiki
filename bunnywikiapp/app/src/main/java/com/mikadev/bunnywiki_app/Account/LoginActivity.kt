d1b1632556d64dfdc52c1563325eb1650de8d500
mwiwema <mwiwema09@gmail.com>
2021-01-20 14:30:47 +0100

    login_activity layout
    * drawable input form
    * deleted rectangle.png drawable
    
    Desktop push

--- bunnywikiapp/app/src/main/java/com/mikadev/bunnywiki_app/Account/LoginActivity.kt
+++ bunnywikiapp/app/src/main/java/com/mikadev/bunnywiki_app/Account/LoginActivity.kt
@@ -1,12 +1,15 @@
-package com.mikadev.bunnywiki_app.bunnywiki_app
+package com.mikadev.bunnywiki_app.account
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mikadev.bunnywiki_app.R
 
 class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
     }
 }
