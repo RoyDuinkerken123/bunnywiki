39b09f711e16a1708b1ffa67143cc07290264d83
RoyDuinkerken123 <Duinkerkenroy1123@gmail.com>
2021-01-20 14:39:31 +0100

    HERSTELD

--- overzichten/overzicht-producten.php
+++ overzichten/overzicht-producten.php
@@ -1,33 +1,8 @@
-<!--Social media koppelingen?-->
-<!--Bunnystuff.nl-->
 <?php
-  Include '..\Database\DB_connect.php'
- ?>
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="../assets/css/style.css">
-  <link rel="stylesheet" href="../assets/css/home.css">
-  <link rel="stylesheet" href="../assets/css/footer.css">
-  <link rel="stylesheet" href="../assets/css/overzichten/producten.css">
-  <link rel="stylesheet" href="../assets/css/header.css">
-  <link rel="stylesheet" href="../assets/css/login_registratie.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-  <title>Bunnywiki</title>
-</head>
-
-<body>
+session_start();
+include "../Database/DB_connect.php";
+include "../header/header.php";
+?>
   <div class="container">
     <div class="row">
       <?php
@@ -97,4 +72,3 @@
       <?php } ?>
     </div>
   </div>
-</body>
--- product_toevoegen/product_toevoegen-pagina.php
+++ product_toevoegen/product_toevoegen-pagina.php
@@ -1,59 +1,7 @@
 <?php
     include 'DB_connect.php';
+    include "../header/header.php";
 ?>
-
-<!--Social media koppelingen?-->
-<!--Bunnystuff.nl-->
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/contact.css">
-    <link rel="stylesheet" href="../assets/css/profiel.css">
-    <link rel="stylesheet" href="../assets/css/wachtwoord-vergeten.css">
-    <link rel="stylesheet" href="../assets/css/home.css">
-    <link rel="stylesheet" href="../assets/css/footer.css">
-    <link rel="stylesheet" href="../assets/css/header.css">
-    <link rel="stylesheet" href="../assets/css/overzichten/biedingen.css">
-    <link rel="stylesheet" href="../assets/css/overzichten/gebruikers.css">
-    <link rel="stylesheet" href="../assets/css/overzichten/producten.css">
-    <link rel="stylesheet" href="../assets/css/login_registratie.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <title>Bunnywiki</title>
-</head>
-<body>
-<div class="container-fluid p-0 header">
-    <div class="container bg-dark">
-        <div class="row justify-content-lg-between justify-content-center">
-            <div class="col-lg-3 p-0">
-                <div class="row justify-content-lg-start justify-content-center">
-                    <div class="logoHolder">
-                        <img src="../assets/images/dummy.jpg" class="logo">
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-5">
-                <div class="row align-items-center h-100 justify-content-center">
-                    <input type="text" id="searchBar" placeholder="Zoek product">
-                </div>
-            </div>
-            <div class="col-lg-2">
-                <div class="row align-items-center h-100 justify-content-lg-start justify-content-end">
-                    <a href="../login/login-pagina.html" id="loginBtn" class="globalBtn border border-primary">
-                        Inloggen
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
 <div class="container p-0">
     <div class="row justify-content-center">
         <div class="col-lg-7 text-white">
@@ -102,8 +50,8 @@
                 </div>
             </div>
             <div class="textHolder col-lg-3">
-                <a href="../login/login-pagina.html"> Inloggen </a>
-                <a href="../login/login-pagina.html"> Gratis registreren </a>
+                <a href="../login/login-pagina.php"> Inloggen </a>
+                <a href="../login/login-pagina.php"> Gratis registreren </a>
             </div>
         </div>
     </div>
