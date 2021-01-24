692b29c5c7cffdbc9cf2e7a0557a8de90eb71eb7
mwiwema <mwiwema09@gmail.com>
2021-01-25 00:02:38 +0100

    Registreer en login activity
    * Layout registreer en login
    * Input icons aangemaakt en toegevoegd
    * Intent wissel tussen de twee activities aangemaakt
    
    Desktop push

--- bunnywikiapp/app/src/main/java/com/mikadev/bunnywiki_app/Account/LoginActivity.kt
+++ bunnywikiapp/app/src/main/java/com/mikadev/bunnywiki_app/Account/LoginActivity.kt
@@ -1,15 +1,43 @@
 package com.mikadev.bunnywiki_app.account
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.mikadev.bunnywiki_app.R
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnTouchListener {
 
+    private lateinit var loginButton: Button
+    private lateinit var registreerLink: TextView
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
 
+        loginButton = findViewById(R.id.button_login)
+        registreerLink = findViewById(R.id.text_noAccount)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegistreerActivity::class.java)
+            startActivity(intent)
+        }
+
+        registreerLink.setOnTouchListener(this)
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        v?.performClick()
+        when (v?.id) {
+            R.id.text_noAccount -> {val intent = Intent(this@LoginActivity, RegistreerActivity::class.java)
+            startActivity(intent) }
+        }
+        return true
     }
 }
+
---
+++ bunnywikiapp/app/src/main/java/com/mikadev/bunnywiki_app/Account/RegistreerActivity.kt
@@ -0,0 +1,13 @@
+package com.mikadev.bunnywiki_app.account
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mikadev.bunnywiki_app.R
+
+class RegistreerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registreer_activity)
+    }
+}
